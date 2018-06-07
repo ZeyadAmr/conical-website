5e4fb6aa28ad9d92bb2a8f3b1b62470b478b1e24
Geoff <you@example.com>
2018-06-08 01:24:22 +0200

    add animations and changed last box

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,5 @@
 var toggler = document.querySelector(".navbar-toggler");
 var navbar = document.querySelector("#navbar-content");
-var myWidth = document.body.clientWidth >= 991;
 
 toggler.addEventListener("click", function () {
   navbar.classList.toggle("h-0");
